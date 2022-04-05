dc6042532c0ee1d0655c761c78bdc05da3222f5e
Zela2520 <sugarbeatzela@gmail.com>
2022-04-05 20:20:47 +0300

    continue to train 3

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 
+# Список словарей
+QUESTIONS = [
+     {
+          "title": f"Title #{i}",
+          "text" : f"This is the text for question #{i}",
+     } for i in range(10)
+]
 
 # Create your views here.
 def index(request):
-     return render(request, "index.html")
+     return render(request, "index.html", {"questions": QUESTIONS})
