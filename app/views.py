70156d708198a7c070d72912f38bc4ee6e45f5dc
Zela2520 <sugarbeatzela@gmail.com>
2022-04-05 21:07:44 +0300

    continue to train 4

--- app/views.py
+++ app/views.py
@@ -4,10 +4,16 @@ from django.shortcuts import render
 QUESTIONS = [
      {
           "title": f"Title #{i}",
-          "text" : f"This is the text for question #{i}",
+          "text": f"This is the text for question #{i}",
      } for i in range(10)
 ]
 
 # Create your views here.
+
+
 def index(request):
      return render(request, "index.html", {"questions": QUESTIONS})
+
+
+def ask(request):
+     return render(request, "ask.html")
