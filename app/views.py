c9ad6224624e53c88082a73a031e7523945265c9
Zela2520 <sugarbeatzela@gmail.com>
2022-04-10 14:26:29 +0300

    learned to work with dynamic data transfer to templates

--- app/views.py
+++ app/views.py
@@ -8,11 +8,19 @@ QUESTIONS = [
      } for i in range(10)
 ]
 
+first_row = {"1": "C/C++", "2": "Python", "3": "SQL"}
+second_row = {"1": "Django", "2": "PHP", "3": "JS"}
+third_row = {"1": "QT", "2": "Ruby", "3": "Java"}
+
+TAGS = [first_row, second_row, third_row]
+
+INDEX_CONTENT = [QUESTIONS, TAGS]
+
 # Create your views here.
 
 
 def index(request):
-     return render(request, "index.html", {"questions": QUESTIONS})
+     return render(request, "index.html", {"index_content": INDEX_CONTENT})
 
 
 def ask(request):
