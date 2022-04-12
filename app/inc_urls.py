1642e233626f1933e841515ce17ead39420634a9
Zela2520 <sugarbeatzela@gmail.com>
2022-04-12 20:41:26 +0300

    listing page ready

--- app/inc_urls.py
+++ app/inc_urls.py
@@ -4,6 +4,6 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('ask/', views.ask),
+    path('ask/', views.ask, name="ask"),
     path('<int:i>/', views.question, name="question")
 ]
