92f27b358199742d9f4189428005c9c2d6f27deb
Zela2520 <sugarbeatzela@gmail.com>
2022-06-15 14:18:37 +0300

    пытаюсь отрендерить данные из БД

--- app/views.py
+++ app/views.py
@@ -1,60 +1,139 @@
-from django.shortcuts import render
-import collections
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from paginator import paginate
+from django.core.paginator import Paginator
+from app.models import *
 
 
 # Обработчик возвращает строку, состоящую из суммы question.number + 1 (инкрементирует question.number, чтобы было красиво, а не с нуля).
-def get_sum_str(i:int, adding_row: int):
+def get_sum_str(i: int, adding_row: int):
     res = i + adding_row
     return str(res)
-
 
 
 def get_array_value(i: int, some_list):
     res = some_list[i]
     return str(res)
 
+
+top_users = Profile.objects.get_top_users(10)
+
+USER = {"is_auth": False}
+
 # Create your views here.
 
 
 def index(request):
-    i = 0
-    return render(request, "index.html", {"index_content": CONTENT, "users": USERS, "question_tags": QUESTIONS_TAGS,
-                                          "get_picture": get_array_value(i, USERS)})
+    questions = Question.objects.new()
+    page_obj = paginate(questions, request, 10)
+    top_tags = Tag.objects.top_tags(10)
+
+    # в блоке ниже группируем данные полученные из БД для их желаемого отображения на странице
+    first_row = {"1": top_tags[0], "2": top_tags[1], "3": top_tags[2]}
+    second_row = {"1": top_tags[3], "2": top_tags[4], "3": top_tags[5]}
+    third_row = {"1": top_tags[6], "2": top_tags[7], "3": top_tags[8]}
+    side_panel_tags = [first_row, second_row, third_row]
+
+    content = {
+        "questions": page_obj,
+        "active_users": top_users,
+        "popular_tags": top_tags,
+        "side_panel_tags": side_panel_tags,
+        "auth": USER['is_auth']
+    }
+
+    return render(request, "index.html", content)
 
 
 def question(request, i: int):
-    return render(request, "question_page.html", {"question": QUESTIONS[i], "iterator": i, "index_content": CONTENT,
-                                                  "answer_id": get_sum_str(1, QUESTIONS[i].get('number', 0)),
-                                                  "get_picture": get_array_value(i, USERS), "question_tags": QUESTIONS_TAGS})
+    content = {
+        "active_users": top_users,
+        "popular_tags": Tag.objects.top_tags(10),
+        "auth": USER['is_auth']
+    }
+    try:
+        question = Question.objects.by_id(i)
+        answers = paginate(Answer.objects.answer_by_question(i), request, 10)
+        content.update({
+            "answers": answers,
+            "question": question,
+        })
+    except Exception:
+        return render(request, "page_not_found.html", content, status=404)
+    return render(request, "question_page.html", content)
 
 
 def ask(request):
-     return render(request, "ask.html", {"index_content": CONTENT})
+    USER['is_auth'] = True
+    content = {
+        "active_users": top_users,
+        "popular_tags": Tag.objects.top_tags(10),
+        "auth": USER['is_auth']
+    }
+    return render(request, "ask.html", content)
 
 
 def login(request):
-     return render(request, "login.html", {"index_content": CONTENT})
+    USER['is_auth'] = True
+    content = {
+        "active_users": top_users,
+        "popular_tags": Tag.objects.top_tags(10),
+        "auth": USER['is_auth']
+    }
+    return render(request, "login.html", content)
 
 
 def registration(request):
-     return render(request, "registration.html", {"index_content": CONTENT})
+    content = {
+        "active_users": top_users,
+        "popular_tags": Tag.objects.top_tags(10),
+        "auth": False
+    }
+    return render(request, "registration.html", content)
 
 
 def settings(request):
-     return render(request, "settings.html", {"index_content": CONTENT})
+    content = {
+        "active_users": top_users,
+        "popular_tags": Tag.objects.top_tags(10),
+        "auth": USER['is_auth']
+    }
+    return render(request, "setting.html", content)
+
+
+def logout(request):
+    USER['is_auth'] = False
+    return index(request)
 
 
 def hot(request):
-    i = 0
-    return render(request, "hot.html", {"index_content": CONTENT, "question_tags": QUESTIONS_TAGS,
-                                        "get_picture": get_array_value(i, USERS)})
+    questions = Question.objects.new()
+    page_obj = paginate(questions, request, 10)
+    top_tags = Tag.objects.top_tags(10)
+    content = {
+        "questions": page_obj,
+        "active_users": top_users,
+        "popular_tags": top_tags,
+        "auth": USER['is_auth']
+    }
+
+    return render(request, "hot.html", content)
 
 
 def tag_listing(request, tag: str):
-    i = 0
-    return render(request, "tag_listing.html", {"index_content": CONTENT, "cur_tag": tag, "question_tags": QUESTIONS_TAGS,
-                                                "get_picture": get_array_value(i, USERS)})
+    content = {
+        "tag": tag,
+        "active_users": top_users,
+        "popular_tags": Tag.objects.top_tags(10),
+        "auth": USER['is_auth']
+    }
+    try:
+        questions = Question.objects.by_tag(tag)
+        page_obj = paginate(questions, request, 10)
+        content.update({
+            "questions": page_obj,
+        })
+    except Exception:
+        return render(request, "page_not_found.html", content, status=404)
 
-
-def paginate(request, object_list, per_page=10):
-    return page
+    return render(request, "tag_listing.html", content)
--- askme_zela/settings.py
+++ askme_zela/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -32,8 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',
-    'app.management',
-    'app.management.commands',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
